da6ad7e412bbfb624ab4ea9f3d7813ecfc19e3d9
Rafael De Luca <deluca1712@gmail.com>
2021-04-26 17:03:01 -0300

    Sign in e Sign out do painel do admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,18 +1,16 @@
 class AdminController < ApplicationController
- 
     before_action :authenticate_admin
-    
-def authenticate admin
-    redirect_to new_admin_session_path unless current_admin?
-
-end
-
-def current_admin?
-   if current_admin.pressent?
-    true
-   else
-    false
+  
+    def authenticate_admin
+      redirect_to new_admin_session_path unless current_admin?
     end
-end
-
-end
+  
+  
+    def current_admin?
+      if current_admin.present?
+        true
+      else
+        false
+      end
+    end
+  end
