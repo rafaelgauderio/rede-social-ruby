3590691602046aa39f7f8464e1648ba88e38fb84
Rafael De Luca <deluca1712@gmail.com>
2021-04-30 10:39:13 -0300

    Criando uma timeline

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
     resources :admins
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "user/timeline#index"
+
+  namespace :user do
+
+  end
 end
