ddc9f2af6209efde1eb612bd8eea3bcc3a20c489
Rafael De Luca <deluca1712@gmail.com>
2021-04-30 16:16:48 -0300

    tela e rotas de cadastro de usuário

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {registrations: 'users/registrations'}
   devise_for :admins, skip: [:registrations], controllers: {sessions: 'admins/sessions'}
 
   namespace :admin do
     root to: "home#index"
     resources :admins
+    resources :users, only: :index
   end
-  
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "user/timeline#index"
-
   namespace :user do
 
-  end
+  end  
 end
